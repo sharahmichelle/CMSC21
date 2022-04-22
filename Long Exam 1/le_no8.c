c3af68ee8f881577314a3bab9a259e0ec01de504
Sharah Michelle Tuando <100456976+sharahmichelle@users.noreply.github.com>
2022-04-22 15:51:24 +0800

    Long Exam 1

---
+++ Long Exam 1/le_no8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main (){
+    // declaration of variables a and b
+    int a = 2, b = 3;
+    // modified if and else statement
+    if ( a == 2 && b == 3 ) {
+        // printing the output
+        printf("a.");
+        printf( "*****\n" );
+        printf( "  >>>>>\n" );
+        printf( "  <<<<<\n" );
+        printf("b.");
+        printf( "*****\n" );
+        printf("c.");
+        printf( "*****\n" );
+        printf( "  <<<<<\n" );
+    }
+    else {
+        printf( "-----\n" );
+    }
+    return 0;
+}
+
